9ec6fae8b51107e249c7fb6cddfca89800e47118
mixsoupe <78546070+mixsoupe@users.noreply.github.com>
2022-05-31 22:19:04 +0200

    Update ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 # -*- encoding: utf8 -*-
-from Adafruit_MCP230xx import *
+#from Adafruit_MCP230xx import *
 
-mcp = Adafruit_MCP230XX(address = 0x20, num_gpios = 16) # MCP23017
+#mcp = Adafruit_MCP230XX(address = 0x20, num_gpios = 16) # MCP23017
 
-mcp.config(5, mcp.INPUT)
-mcp.config(6, mcp.INPUT)
-mcp.config(7, mcp.INPUT)
+#mcp.config(5, mcp.INPUT)
+#mcp.config(6, mcp.INPUT)
+#mcp.config(7, mcp.INPUT)
 #mcp.pullup(3, 1)
 
 while (True):
     #print (mcp.input(5))
+    print ("test")
     time.sleep(1)
