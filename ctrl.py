f5403dee5b8933218d8ab2fb47581747ddbffba2
mixsoupe <78546070+mixsoupe@users.noreply.github.com>
2022-05-31 22:15:19 +0200

    Update ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -10,7 +10,5 @@ mcp.config(7, mcp.INPUT)
 #mcp.pullup(3, 1)
 
 while (True):
-    print ("Pin 5 = %d" % (mcp.input(5) >> 5)
-    print ("Pin 6 = %d" % (mcp.input(6) >> 6)
-    print ("Pin 7 = %d" % (mcp.input(7) >> 7)
+    print (mcp.input(5))
     time.sleep(1)
