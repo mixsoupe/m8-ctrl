4739d752317a221cf2322662f37d83c76d22e639
mixsoupe <78546070+mixsoupe@users.noreply.github.com>
2022-06-01 00:03:57 +0200

    Update ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -25,10 +25,7 @@ for pin in port_a_pins:
 
 
 while (True):
-    for num, led in enumerate(port_a_pins):
-        #print (pin4.value)
-        port_a_pins[num].value = True  # turn LED on!
-
-        time.sleep(1)
-        port_a_pins[num].value = True  # turn LED off
+    port_a_pins[0].value = True
+    time.sleep(1)
+    port_a_pins[0].value = True
 
