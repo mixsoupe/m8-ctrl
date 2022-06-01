d6e3de1eaea8b39ff41e0a4c4e2435ea43d5c34c
mixsoupe <78546070+mixsoupe@users.noreply.github.com>
2022-06-01 21:41:49 +0200

    Update ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -8,11 +8,11 @@ from digitalio import Direction, Pull
 
 from adafruit_extended_bus import ExtendedI2C as I2C
 
-from adafruit_mcp230xx.mcp23016 import MCP23016
+from adafruit_mcp230xx.mcp23017 import MCP23017
 
 i2c = I2C(11)
 
-mcp = MCP23016(i2c)
+mcp = MCP23017(i2c)
 
 pins = []
 #for pin in range(0, 15):
@@ -26,6 +26,7 @@ pin0.direction = Direction.OUTPUT
 
 pin7 = mcp.get_pin(7)
 pin7.direction = Direction.INPUT
+pin7.pull = Pull.PULLUP
     
 while (True):
     print (pin7.value)
