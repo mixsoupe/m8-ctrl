2bdcf14d063edec7ddecc5ccb88b2e6e53a274b8
mixsoupe <78546070+mixsoupe@users.noreply.github.com>
2022-06-01 21:15:57 +0200

    Update ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -15,24 +15,23 @@ i2c = I2C(11)
 mcp = MCP23016(i2c)
 
 pins = []
-#for pin in range(1, 16):
+#for pin in range(0, 15):
     #pins.append(mcp.get_pin(pin))
 
 # Set all the port A pins to output
 #for pin in pins:
     #pin.direction = Direction.OUTPUT
-pin7 = mcp.get_pin(0)
-pin7.direction = Direction.OUTPUT
+pin0 = mcp.get_pin(0)
+pin0.direction = Direction.OUTPUT
+
+pin7 = mcp.get_pin(7)
+pin7.direction = Direction.INPUT
     
 while (True):
-    pin7.value = True
-    #pins[7].value = True
-    #print("True")
-    time.sleep(1)
-    #pins[7].value = False
-    #print("False")
-    #time.sleep(0.5)
-    pin7.value = False
+    print (pin7.value)
+    pin0.value = True
+    time.sleep(.5)
+    pin0.value = False
     
-    time.sleep(1)
+    time.sleep(.5)
 
