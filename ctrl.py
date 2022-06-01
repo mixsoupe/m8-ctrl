6f562f6e7930b6c72a11117da51e2966a426098f
mixsoupe <78546070+mixsoupe@users.noreply.github.com>
2022-06-01 22:42:59 +0200

    Update ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python
 # -*- encoding: utf8 -*-
 import time
-
-import uinput
-
+from pynput.keyboard import Key, Controller
 import board
 import busio
 from digitalio import Direction, Pull
-
 from adafruit_extended_bus import ExtendedI2C as I2C
-
 from adafruit_mcp230xx.mcp23017 import MCP23017
+
+keyboard = Controller()
 
 i2c = I2C(11)
 
@@ -19,20 +17,11 @@ mcp = MCP23017(i2c)
 pin7 = mcp.get_pin(7)
 pin7.direction = Direction.INPUT
 pin7.pull = Pull.UP
-
-events = (
-    uinput.KEY_E,
-    uinput.KEY_H,
-    uinput.KEY_L,
-    uinput.KEY_O,
-    )
-    
+  
 while (True):
     if not pin7.value:
         #print ("key 1")
         time.sleep(1)
-        with uinput.Device(events) as device:
-            print ("key pressed")
-            device.emit_click(uinput.KEY_E)
+        keyboard.press('A')
     
 
